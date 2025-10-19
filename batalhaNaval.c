670e6c106623738c09b97d925bf5f095b568b1dd
Genivaldo Santos de Oliveira <genoliveira@live.com>
2025-10-18 22:03:41 -0300

    Update batalhaNaval.c
    melhorando o codigo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -14,12 +14,12 @@ int main() {
 
     // Navio horizontal de tamanho 3 na linha 2
     for (int j = 2; j < 5; j++) {
-        tabuleiro[2][j] = '7';  //navio representa o numero 7
+        tabuleiro[2][j] = '3';  //navio representa o numero 3
     }
 
     // Navio vertical de tamanho 3 na coluna 5
     for (int i = 5; i < 8; i++) {
-        tabuleiro[i][5] = '7';  //navio representa o numero 7
+        tabuleiro[i][5] = '3';  //navio representa o numero 3
     }
 
     // Mostra o tabuleiro
